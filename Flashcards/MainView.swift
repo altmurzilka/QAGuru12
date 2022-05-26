7ac46ad1538e8750a633fccf61b33b2af6c4380f
altmurzilka <altmurzilka@gmail.com>
2022-05-26 22:46:49 +0600

    new

--- Flashcards/MainView.swift
+++ Flashcards/MainView.swift
@@ -4,25 +4,25 @@ import Lottie
 
 // Main view screen
 // Yet another screen
-struct MainView: View {
-    @State private var showContentView = false
-    @State private var cards: [Card] = []
-    
-    @Environment(\.managedObjectContext) private var viewContext
-    var body: some View {
-        if showContentView {
-            FlashCardsView(cards: $cards, showContentView: $showContentView)
-                .environment(\.managedObjectContext, viewContext)
-            
-        } else {
-            VStack(spacing: 10) {
-                SplashScreen()
-                CardDeckView(selectedCards: $cards, showContentView: $showContentView)
-                    .environment(\.managedObjectContext, viewContext)
-            }
-        }
-    }
-}
+//struct MainView: View {
+//    @State private var showContentView = false
+//    @State private var cards: [Card] = []
+//    
+//    @Environment(\.managedObjectContext) private var viewContext
+//    var body: some View {
+//        if showContentView {
+//            FlashCardsView(cards: $cards, showContentView: $showContentView)
+//                .environment(\.managedObjectContext, viewContext)
+//            
+//        } else {
+//            VStack(spacing: 10) {
+//                SplashScreen()
+//                CardDeckView(selectedCards: $cards, showContentView: $showContentView)
+//                    .environment(\.managedObjectContext, viewContext)
+//            }
+//        }
+//    }
+//}
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
